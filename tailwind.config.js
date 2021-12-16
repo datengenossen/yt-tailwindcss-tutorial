5d2f0bbb8af56b3acade7d2c482167f3b80191fb
Jürgen Schadek <56023985+Juergen-Schadek@users.noreply.github.com>
2021-12-16 14:27:23 +0100

    Farben und Schriften

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./**/index.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Arimo', 'sans-serif'],
+      serif: ['Trocchi', 'serif'],
+      mono: ['"Roboto Mono"', 'monospace'],
+      heading: ['"Special Elite"', 'cursive'],
+    },
+    extend: {
+      colors: {
+        'dg-primary': '#335E80',
+        'dg-secondary': '#234159',
+        'dg-base': '#333333',
+        'dg-accent': '#337180',
+        'dg-yellow': '#ffce31',
+      },
+    },
   },
   plugins: [],
 }
